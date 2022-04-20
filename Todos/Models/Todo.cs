9d9e736359914af2ffb1d59961e5870b25939996
olcay <olcay.parlak@gmail.com>
2022-04-20 16:05:35 +0300

    first commit olcay

---
+++ Todos/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace Todos.Models
+{
+    public class Todo
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }   
+        public bool Completed { get; set; } 
+    }
+}
